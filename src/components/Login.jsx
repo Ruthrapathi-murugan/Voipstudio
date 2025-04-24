b64263c05103cfe276401aec9481d4b125b1d934
Ruthrapathi-murugan <ruthrapathimurugan@outlook.com>
2025-04-25 02:15:59 +0530

    final check tis

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -21,16 +21,12 @@ const Login = () => {
         <div className="hero-section">
           <div className="hero-content">
             <h1 className="hero-title">
-              <span className="gradient-text">AI</span> Powered <br />
-              <span className="gradient-text">Voice Revolution</span>
+              <span className="gradient-text">Welcome</span> Back <br />
+              <span className="gradient-text">to Voip Studio</span>
             </h1>
             <p className="hero-subtitle">
-              Deliver smarter, faster, human-like voice interactions with AI-powered LLMs for natural, advanced conversations.
+            Login to manage your calls, APIs, and AI workflows.
             </p>
-            <div className="hero-buttons">
-              <button className="gradient-btn">Try the Demo</button>
-              <button className="gradient-btn-outline">View API Docs</button>
-            </div>
           </div>
         </div>
 
@@ -39,7 +35,7 @@ const Login = () => {
           <div className="login-overlay"></div>
           <div className="login-card">
             <h1 className="login-title">Welcome Back to Voip Studio</h1>
-            <p className="login-subtitle">Login to manage your calls, APIs, and AI workflows.</p>
+         
             
             <div className="divider"></div>
             
@@ -70,7 +66,7 @@ const Login = () => {
               <span>or</span>
             </div>
             
-            <div className="social-logins">
+           <div className="social-logins">
   <button className="social-button github">
     <svg className="social-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
       <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/>
